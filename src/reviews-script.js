8bf5d068c67bc202a0f8dec1c26225c88daa0de5
Konovalova-Kateryna <kytana2112@gmail.com>
2022-04-05 20:27:04 +0300

    Настроен слайдер
    Исправлен/настроен слайдер, удалены лишние комментарии, в индекс-файл добавлены скрипты для слайдера

--- src/reviews-script.js
+++ src/reviews-script.js
@@ -1,9 +1,13 @@
-$(document).ready(function () {
-    $('.slider').slick({
+$('.slider').slick({
         arrows:false,
         dots:true,
         adaptiveHeight:true,
         slidesToShow: 1,
         autoplay:false
-    });
 });
+
+
+
+
+
+  
